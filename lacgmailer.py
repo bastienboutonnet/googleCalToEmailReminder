72a63a5e540dd521db859552d0e20044f064e2a8
bastienboutonnet <bastien.b1@gmail.com>
2016-01-10 12:42:30 +0100

    makes mailer silent

--- lacgmailer.py
+++ lacgmailer.py
@@ -55,7 +55,7 @@ def SendMessage(service, user_id, message):
 	"""
 	message = (service.users().messages().send(userId=user_id, body=message)
 						 .execute())
-	print 'Message Id: %s' % message['id']
+	#print 'Message Id: %s' % message['id']
 	return message
 
 def CreateMessage(sender, to, subject, message_text):
